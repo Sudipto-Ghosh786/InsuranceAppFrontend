6ff722ac723e5941282a01487f888736cf2822cf
Sudipto-Ghosh786 <sudiptoghosh7869@gmail.com>
2023-11-14 11:38:05 -0800

    Made minor bug fix for add to cart button component.

--- src/components/PolicyCard.jsx
+++ src/components/PolicyCard.jsx
@@ -1,7 +1,10 @@
-import React from 'react';
-import { FaShoppingCart } from 'react-icons/fa';
+import React from "react";
+import { useState } from "react";
+import { FaShoppingCart } from "react-icons/fa";
 
 export default function PolicyCard({ title, description, imageUrl }) {
+  const [isHovered, setIsHovered] = useState(false);
+
   return (
     <div className="m-4 flex flex-col text-gray-700 bg-white shadow-md w-96 rounded-xl bg-clip-border">
       <div className="p-6">
@@ -21,14 +24,19 @@ export default function PolicyCard({ title, description, imageUrl }) {
           Show Details
         </button>
         <button
-          className="add-to-cart-button select-none rounded-lg bg-pink-500 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-pink-500/20 transition-all relative overflow-hidden"
+          className="add-to-cart-button select-none rounded-lg bg-pink-500 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-pink-500/20 transition-all hover:shadow-lg hover:shadow-pink-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none flex justify-center items-center"
           type="button"
           data-ripple-light="true"
+          onMouseEnter={() => setIsHovered(true)}
+          onMouseLeave={() => setIsHovered(false)}
         >
-          <span className="text-content">Add To Cart</span>
-          <span className="icon-content">
-            <FaShoppingCart />
-          </span>
+          {isHovered ? (
+            <span className="icon-content opacity-0">
+              <FaShoppingCart />
+            </span>
+          ) : (
+            <span className="text-content">Add To Cart</span>
+          )}
         </button>
       </div>
     </div>
